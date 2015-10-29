a9d39f3000257148b058b58b98eddd031af7a08f
jayamurugandoodle <jayamurugan@doodleblue.com>
2015-10-29 15:44:03 +0530

    Sample
    New Changes

---
+++ Calendar-One/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calendar-One
+//
+//  Created by jayamurugan on 8/31/15.
+//  Copyright (c) 2015 jayamurugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calendar-One/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calendar-One
+//
+//  Created by jayamurugan on 8/31/15.
+//  Copyright (c) 2015 jayamurugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
